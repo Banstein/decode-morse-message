d509327fe6365b293e4def9fb95a37643b68c261
Banstein <islambahnas1@gmail.com>
2022-05-12 15:09:14 +0200

    Add word, phrase methods

--- app.rb
+++ app.rb
@@ -1,14 +1,33 @@
-def decode_morse(morse_char)
-  morse_code = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
-    '-..' => 'D', '.' => 'E', '..-.' => 'F',
-    '--.' => 'G', '....' => 'H', '..' => 'I',
-    '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
-    '--' => 'M', '---' => 'O', '.--.' => 'P',
-    '--.-' => 'Q', '.-.' => 'R', '...' => 'S',
-    '-' => 'T', '..-' => 'U', '...-' => 'V',
-    '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-    '--..' => 'Z'
+@morse_code = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
+  '-..' => 'D', '.' => 'E', '..-.' => 'F',
+  '--.' => 'G', '....' => 'H', '..' => 'I',
+  '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '---' => 'O', '.--.' => 'P',
+  '--.-' => 'Q', '.-.' => 'R', '...' => 'S',
+  '-' => 'T', '..-' => 'U', '...-' => 'V',
+  '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z'
   }
-  morse_code[morse_char]
+
+def decode_char(morse_char)
+  @morse_code[morse_char]
 end
+
+def decode_word(word)
+  trs_word = ''
+  word.split.each do |char|
+    trs_word.concat(decode_char(char))
+  end
+  trs_word
+end
+
+def decode(phrase)
+  trs_phrase = ''
+  phrase.split('   ').each do |word|
+    trs_phrase.concat(decode_word(word)).concat(' ')
+  end
+  trs_phrase
+end
+
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
